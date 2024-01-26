7c79f53305f716e0786605016f59d917f46b2960
Luis Roa <luis.roa@monday-consulting.com>
2024-01-26 16:29:28 +0100

    implement courses component and template

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -13,12 +13,33 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
       favorite: true
+    },{
+      id: 2,
+      title: 'TypeScript Fundamentals',
+      description: 'Learn the fundamentals of TypeScript',
+      percentComplete: 0,
+      favorite: false
+    },{
+      id: 3,
+      title: 'Intermediate Angular 13',
+      description: 'Implement an Angular 13 App',
+      percentComplete: 0,
+      favorite: false
     }
   ];
+  selectedCourse = null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course) {
+    this.selectedCourse = course;
+  }
+
+  deleteCourse(id) {
+    console.log('Deleting course with id', id);
   }
 
 }
